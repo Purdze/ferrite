f9e2b546758a6856105550654d4a41f2cb47bfa8
arse09 <arse.dc.09@gmail.com>
2026-04-10 02:47:13 +0200

    Mount builder events

--- launcher/src-tauri/src/main.rs
+++ launcher/src-tauri/src/main.rs
@@ -13,16 +13,18 @@ fn main() {
     #[cfg(debug_assertions)]
     pomme_launcher::generate_bindings();
 
+    let invoke_handler = builder.invoke_handler();
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .setup(|app| {
+        .setup(move |app| {
+            builder.mount_events(app);
             pomme_launcher::storage::ensure_dirs();
             app.manage(pomme_launcher::AppState::default());
             Ok(())
         })
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
-        .invoke_handler(builder.invoke_handler())
+        .invoke_handler(invoke_handler)
         .run(tauri::generate_context!())
         .expect("failed to run Pomme launcher");
 }
